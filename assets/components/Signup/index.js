706449ca6c99dd5884cac37ba4ca422ec4cd494f
Liza-T-XLS <shapi94@gmail.com>
2021-01-10 02:24:39 +0100

    Signup component: triggered all checkErrors onSubmit in case a user would submit without ever modifying the fields, this way, the user gets the error msgs + installation of classnames to manage invalid fields + related CSS

--- assets/components/Signup/index.js
+++ assets/components/Signup/index.js
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './signup.scss';
 import visibleIcon from '../../images/visible-24.png';
@@ -22,18 +23,26 @@ const Signup = ({
   checkErrors,
   formErrors,
 }) => {
+  const pseudonymClassName = classNames('pseudonym', { invalid: formErrors.pseudonym.length > 0 });
+  const emailClassName = classNames('email', { invalid: formErrors.email.length > 0 });
+  const passwordClassName = classNames('password', { invalid: formErrors.password.length > 0 });
+  const confirmedPasswordClassName = classNames('confirmedPassword', { invalid: formErrors.confirmedPassword.length > 0 });
+
   const onChangeHandler = (e) => {
     console.log('onChangeHandler triggered: ' + e.target.value + ' for: ' + e.target.name);
     changeField(e.target.value, e.target.name);
     checkErrors(e.target.name);
   };
-
   const onSubmitHandler = (e) => {
     e.preventDefault();
     console.log('onSubmitHandler triggered');
+    checkErrors('pseudonym');
+    checkErrors('email');
+    checkErrors('password');
+    checkErrors('confirmedPassword');
     const errors = Object.values(formErrors).find((value) => value.length > 0);
     console.log(Object.values(formErrors));
-    if (errors === undefined) {
+    if (errors === undefined && pseudonym !== '' && email !== '' && password !== '' && confirmedPassword !== '') {
       console.log('form submitted');
     }
   };
@@ -44,24 +53,23 @@ const Signup = ({
       <form className="signupForm" onSubmit={onSubmitHandler} noValidate>
         <label htmlFor="pseudonym">
           <span>Pseudonym</span>
-          <input type="text" name="pseudonym" id="pseudonym" value={pseudonym} onChange={onChangeHandler} minLength="2" required />
-          {/* {formErrors.pseudonym.length > 0 && <span className="errorMsg">{formErrors.pseudonym}</span>} */}
+          <input type="text" name="pseudonym" id="pseudonym" value={pseudonym} onChange={onChangeHandler} minLength="2" required className={pseudonymClassName} />
           <div className="errorMsg">{[formErrors.pseudonym].length > 0 && <span>{formErrors.pseudonym}</span>}</div>
         </label>
         <label htmlFor="email">
           <span>Email</span>
-          <input type="email" name="email" id="email" value={email} onChange={onChangeHandler} required />
+          <input type="email" name="email" id="email" value={email} onChange={onChangeHandler} required className={emailClassName} />
           <div className="errorMsg">{[formErrors.email].length > 0 && <span>{formErrors.email}</span>}</div>
         </label>
         <label htmlFor="password">
           <span>Enter a password</span>
-          <input type="password" name="password" id="password" value={password} onChange={onChangeHandler} minLength="4" required />
+          <input type="password" name="password" id="password" value={password} onChange={onChangeHandler} minLength="4" required className={passwordClassName} />
           <img className="passwordToggle" src={visibleIcon} alt="password toggle" onClick={passwordVisibilityOnClickHandler} />
           <div className="errorMsg">{[formErrors.password].length > 0 && <span>{formErrors.password}</span>}</div>
         </label>
         <label htmlFor="confirmedPassword">
           <span>Confirm your Password</span>
-          <input type="password" name="confirmedPassword" id="confirmedPassword" value={confirmedPassword} onChange={onChangeHandler} required />
+          <input type="password" name="confirmedPassword" id="confirmedPassword" value={confirmedPassword} onChange={onChangeHandler} required className={confirmedPasswordClassName} />
           <img className="passwordToggle" src={visibleIcon} alt="password toggle" onClick={passwordVisibilityOnClickHandler} />
           <div className="errorMsg">{[formErrors.confirmedPassword].length > 0 && <span>{formErrors.confirmedPassword}</span>}</div>
         </label>
