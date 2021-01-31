ce10ad651d870b969d993c35e315ed9c253d61e6
Liza-T-XLS <shapi94@gmail.com>
2021-01-31 20:08:08 +0100

    Quote details: displaying tags

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
 use DateTime;
 
 /**
@@ -18,12 +19,14 @@ class Tag
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"quotes_get"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
+     * @Groups({"quotes_get"})
      */
     private $name;
 
