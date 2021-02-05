781f8806c4c9cb10fe080a2683fb3c8884c1604b
Liza-T-XLS <shapi94@gmail.com>
2021-02-05 18:37:14 +0100

    Api_quotes_edit (PUT) created

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -30,15 +30,15 @@ class QuoteController extends AbstractController
                     "characterName": "Smith",
                     "mediumTitle": "The day"
                 },
-                "tags": {
+                "tags": [
                     "joy",
                     "sadness",
-                }
+                    ]
                 }
         */
         $data = json_decode($request->getContent());
 
-        $quote= $denormalizer->denormalize($data->quote, Quote::class);
+        $quote = $denormalizer->denormalize($data->quote, Quote::class);
         $errors = $validator->validate($quote);
 
         $jsonErrors = [];
@@ -122,5 +122,108 @@ class QuoteController extends AbstractController
         $quotes = $quoteRepository->loadQuotesByUserAndPagination($userId, $maxResults, $offset);
 
         return $this->json(['pageQuantity' => $pageQuantity, 'quotes' => $quotes], 200, [], ['groups' => 'quotes_get']);
+    }
+
+    /**
+     * @Route("/api/quotes", name="api_quotes_edit", methods={"PUT"})
+     */
+    public function edit(Request $request, DenormalizerInterface $denormalizer, ValidatorInterface $validator, QuoteRepository $quoteRepository, EntityManagerInterface $entityManager): Response
+    {
+         /* 
+            expected data format:
+                {
+                "quote": {
+                    "id": 5,
+                    "text": "What a beautiful day!",
+                    "authorFirstName": "John",
+                    "authorLastName": "Doe",
+                    "characterName": "Smith",
+                    "mediumTitle": "The day"
+                },
+                "tags": [
+                    "joy",
+                    "sadness",
+                    ]
+                }
+        */
+        $data = json_decode($request->getContent());
+
+        $quote = $quoteRepository->find($data->quote->id);
+
+        if(!$quote) {
+            return $this->json(['message' => 'This quote does not exist.'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = $this->getUser();
+
+        if(!$user) {
+            return $this->json(['message' => 'User not found. Must be connected in order to modify a quote.'], Response::HTTP_UNPROCESSABLE_ENTITY);
+        };
+
+        if($user != $quote->getUser()) {
+            return $this->json(['message' => 'You are not authorized to modify a quote.'], Response::HTTP_FORBIDDEN);
+        }
+
+        // if the quote exists according to the id, we check for errors in the received data
+        $updatedQuote = $denormalizer->denormalize($data->quote, Quote::class);
+        $errors = $validator->validate($updatedQuote);
+
+        $jsonErrors = [];
+
+        if (count($errors) !== 0) {
+            foreach ($errors as $error) {
+                $jsonErrors[] = [
+                    'field' => $error->getPropertyPath(),
+                    'message' => $error->getMessage(),
+                ];
+            }
+        }
+
+        if(!empty($jsonErrors)){
+            return $this->json($jsonErrors, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // if there are no errors the quote is updated
+        $quote->setText($data->quote->text)
+        ->setAuthorFirstName($data->quote->authorFirstName)
+        ->setAuthorLastName($data->quote->authorLastName)
+        ->setCharacterName($data->quote->characterName)
+        ->setMediumTitle($data->quote->mediumTitle);
+        
+        // old tags are removed in order to add new ones
+        $oldTags = $quote->getTags();
+
+        foreach($oldTags as $tag) {
+            $quote->removeTag($tag);
+        }
+
+        $tags = $data->tags;
+        foreach($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->setName($tag);
+            $errors = $validator->validate($newTag);
+
+            $jsonErrors = [];
+
+            if (count($errors) !== 0) {
+                foreach ($errors as $error) {
+                    $jsonErrors[] = [
+                        'field' => $error->getPropertyPath(),
+                        'message' => $error->getMessage(),
+                    ];
+                }
+            }
+
+            if(!empty($jsonErrors)){
+                return $this->json($jsonErrors, Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            
+            $entityManager->persist($newTag);
+            $quote->addTag($newTag);
+        }
+        
+        $entityManager->flush();
+
+        return $this->json(['message' => 'Quote updated'], Response::HTTP_OK);
     }
 }
