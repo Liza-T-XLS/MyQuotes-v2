82934495c60bb9e443a3066affc3f0c3ad26c896
Liza-T-XLS <shapi94@gmail.com>
2021-02-07 19:50:58 +0100

    Nav component: menu and options (when open, slides from left to right)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MyQuotes-v2
+
+Any group of words, short or long, can be a quote. Some quotes are popular, some aren't. But they got **your** attention!
+
+MyQuotes enables you to save any quote to your personal space. They are **your** quotes!
--- assets/components/Header/Nav/index.js
+++ assets/components/Header/Nav/index.js
@@ -5,6 +5,7 @@
 
 import React, { useState } from 'react';
 import classNames from 'classnames';
+import { Link } from 'react-router-dom';
 
 import './nav.scss';
 
@@ -14,14 +15,28 @@ const Nav = () => {
   const [open, setOpen] = useState(false);
 
   const burgerCSS = classNames('burger', { active: open });
+  const navCSS = classNames('nav', { active: open });
+
   const burgerOnClickHandler = () => {
     setOpen(!open);
   };
 
+  const optionOnClickHandler = () => {
+    setOpen(!open);
+  };
+
   return (
-    <nav className="nav">
+    <nav className={navCSS}>
       <div className={burgerCSS} onClick={burgerOnClickHandler}>
         <span />
+      </div>
+      <div className="menu">
+        <ul className="options">
+          <li className="option"><Link to="/" onClick={optionOnClickHandler}>Home</Link></li>
+          <li className="option"><Link to="/login" onClick={optionOnClickHandler}>Log in</Link></li>
+          <li className="option"><Link to="/signup" onClick={optionOnClickHandler}>Sign up</Link></li>
+          <li className="option">About</li>
+        </ul>
       </div>
     </nav>
   );
