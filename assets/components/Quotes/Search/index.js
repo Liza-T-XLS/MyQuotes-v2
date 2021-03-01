9bfa95bca6a60006fbfa2924b2dea7e614630a2f
Liza-T-XLS <shapi94@gmail.com>
2021-03-01 18:35:41 +0100

    Search subcomponent: added resetSearchIcon + stored filter colors into variables

--- assets/components/Quotes/Search/index.js
+++ assets/components/Quotes/Search/index.js
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 // == Imports
 
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import './search.scss';
+import resetSearchIcon from '../../../images/close-thin-18dp.svg';
 
 // == Component
 
@@ -16,12 +19,19 @@ const Search = ({ searchInput, saveSearchInput, loadQuotes, saveCurrentPage }) =
     loadQuotes();
   };
 
+  const resetSearchOnClickHandler = () => {
+    console.log('resetSearchOnChangeHandler');
+  };
+
   return (
     <>
       <label htmlFor="search">
         <fieldset>
           <legend>Find a quote</legend>
-          <input className="searchInput" name="search" value={searchInput} onChange={onChangeHandler} id="search" minLength="1" placeholder="e.g. word, author's name etc." />
+          <div className="inputDiv">
+            <input className="searchInput" name="search" value={searchInput} onChange={onChangeHandler} id="search" minLength="1" placeholder="e.g. word, author's name etc." />
+            <img className="resetSearch" src={resetSearchIcon} alt="reset search icon" title="reset search" onClick={resetSearchOnClickHandler} />
+          </div>
         </fieldset>
       </label>
     </>
