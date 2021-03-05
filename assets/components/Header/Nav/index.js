09b06f7c7f05ba0d13417c33df8d5d75b1e39f49
Liza-T-XLS <shapi94@gmail.com>
2021-03-05 18:29:55 +0100

    My quotes link added to the menu

--- assets/components/Header/Nav/index.js
+++ assets/components/Header/Nav/index.js
@@ -10,6 +10,7 @@ import PropTypes from 'prop-types';
 
 import './nav.scss';
 import homeIcon from '../../../images/home.svg';
+import quoteIcon from '../../../images/quote-36dp.svg';
 import loginIcon from '../../../images/login-36dp.svg';
 import signupIcon from '../../../images/signup.svg';
 import aboutIcon from '../../../images/about-36dp.svg';
@@ -46,6 +47,12 @@ const Nav = ({
             <img src={homeIcon} alt="menu home icon" className="menuIcon" />
             <Link to="/" onClick={burgerOnClickHandler} className="link">Home</Link>
           </li>
+          {isLogged && (
+            <li className="option">
+              <img src={quoteIcon} alt="menu quote icon" className="menuIcon" />
+              <Link to="/quotes" onClick={burgerOnClickHandler} className="link">My quotes</Link>
+            </li>
+          )}
           {!isLogged && (
             <>
               <li className="option">
